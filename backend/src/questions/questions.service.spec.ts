88ae8064e4eddd0f85b9e71eee9e3759c1a3cae5
Victor Lebrun <victorl@theodo.fr>
2019-06-19 19:09:59 +0200

    Fix npm run start prod & dev

--- backend/src/questions/questions.service.spec.ts
+++ backend/src/questions/questions.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsService } from './questions.service';
 
 describe('QuestionsService', () => {
-  let service: QuestionsService;
+    let service: QuestionsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [QuestionsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [QuestionsService],
+        }).compile();
 
-    service = module.get<QuestionsService>(QuestionsService);
-  });
+        service = module.get<QuestionsService>(QuestionsService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
