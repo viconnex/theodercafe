b5b7f7f4b832d7cb7e1bbf512fad28ba3606c15b
Victor Lebrun <victorl@theodo.fr>
2019-06-20 13:55:26 +0200

    [DEVOPS] Test node env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
+    console.log(process.env.NODE_ENV);
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
