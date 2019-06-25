e6a50977cd6ef11d1aa04e33f6041a9c1bc4dc7b
Victor Lebrun <victorl@theodo.fr>
2019-06-26 00:27:31 +0200

    Add tollbar with logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles';
 import './App.css';
+import logo from './ui/logo/theodercafe.png';
 import RandomQuestion from './components/RandomQuestion';
 
-function App() {
+const style = {
+  root: {
+    background: 'white',
+  },
+};
+
+const App = props => {
   return (
     <div className="App">
+      <AppBar classes={props.classes} position="fixed">
+        <ToolBar>
+          <img src={logo} alt="logo" height="20" />
+        </ToolBar>
+      </AppBar>
       <header className="App-header">
         <RandomQuestion />
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default withStyles(style)(App);
