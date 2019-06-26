83921bbcf3be61b8b65d0621bd21d028cad5a3e4
Victor Lebrun <victorl@theodo.fr>
2019-06-27 01:00:14 +0200

    try config for dist prod

--- backend/src/questions/interfaces/questions.dto.js
+++ backend/src/questions/interfaces/questions.dto.js
@@ -1,8 +1,7 @@
-"use strict";
+'use strict';
 exports.__esModule = true;
-var QuestionsDto = /** @class */ (function () {
-    function QuestionsDto() {
-    }
+var QuestionsDto = /** @class */ (function() {
+    function QuestionsDto() {}
     return QuestionsDto;
-}());
+})();
 exports.QuestionsDto = QuestionsDto;
