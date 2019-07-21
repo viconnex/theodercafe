29c462537d5c4c552a62b54f3a5ba966cb6a3b00
Victor Lebrun <victorl@theodo.fr>
2019-07-21 11:13:07 +0200

    Add chien abime

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import img from 'ui/img/chien_abime.jpg';
+import './style.css';
 
 const About = () => {
-  return <div>hey</div>;
+  return (
+    <div id="viewport">
+      <img className="img-about" id="img-big" src={img} />
+      <img className="img-about" id="img-small" src={img} />
+    </div>
+  );
 };
 
 export default About;
