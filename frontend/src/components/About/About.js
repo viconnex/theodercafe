c0a20ba8c9b70114e8ef1a00e01277c182b10350
Victor Lebrun <victorl@theodo.fr>
2019-07-21 11:32:51 +0200

    Add alt

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -5,8 +5,8 @@ import './style.css';
 const About = () => {
   return (
     <div id="viewport">
-      <img className="img-about" id="img-big" src={img} />
-      <img className="img-about" id="img-small" src={img} />
+      <img alt="abime_le_chien" className="img-about" id="img-big" src={img} />
+      <img alt="abime_le_chien" className="img-about" id="img-small" src={img} />
     </div>
   );
 };
