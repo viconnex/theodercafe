91aa0dccacf95f534cb3a0f8133d3e83f899c179
Victor Lebrun <victorl@theodo.fr>
2019-07-24 14:44:44 +0200

    Improve a-propos

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -4,9 +4,11 @@ import './style.css';
 
 const About = () => {
   return (
-    <div id="viewport">
-      <img alt="abime_le_chien" className="img-about" id="img-big" src={img} />
-      <img alt="abime_le_chien" className="img-about" id="img-small" src={img} />
+    <div className="img-container">
+      <div id="viewport">
+        <img alt="abime_le_chien" className="img-about" id="img-big" src={img} />
+        <img alt="abime_le_chien" className="img-about" id="img-small" src={img} />
+      </div>
     </div>
   );
 };
