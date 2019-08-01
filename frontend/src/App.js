7aba9b6e01144a9ef652be5ddb61b714c0fe677e
Victor Lebrun <victorl@theodo.fr>
2019-08-01 09:29:12 +0200

    Change admin url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = ({ classes }) => {
           </AppBar>
           <Switch>
             <Route exact path="/a-propos" component={About} />
-            <Route exact path="/tableau" component={Admin} />
+            <Route exact path="/admin" component={Admin} />
             <Route path="/" component={Questioning} />
           </Switch>
           <Drawer open={isDrawerOpen} toggleDrawer={toggleDrawer} />
