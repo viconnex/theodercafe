b817fde059238093451eb3a9ae094c9292066e91
Victor Lebrun <victorl@theodo.fr>
2019-08-01 16:46:21 +0200

    Add category creation in admin

--- frontend/src/admin/Admin.js
+++ frontend/src/admin/Admin.js
@@ -4,12 +4,13 @@ import jsonServerProvider from 'ra-data-json-server';
 import { API_BASE_URL } from 'utils/constants';
 import { QuestionList } from 'components/QuestionList';
 import { QuestionEdit } from 'components/QuestionEdit';
+import { CategoryCreate } from 'components/CategoryCreate';
 
 const dataProvider = jsonServerProvider(API_BASE_URL);
 const Admin = () => (
   <ReactAdmin dataProvider={dataProvider}>
     <Resource name="questions" list={QuestionList} edit={QuestionEdit} />
-    <Resource name="categories" list={ListGuesser} edit={EditGuesser} />
+    <Resource name="categories" list={ListGuesser} edit={EditGuesser} create={CategoryCreate} />
   </ReactAdmin>
 );
 
---
+++ frontend/src/components/CategoryCreate/CategoryCreate.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Create, SimpleForm, TextInput } from 'react-admin';
+
+export const CategoryCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+    </SimpleForm>
+  </Create>
+);
