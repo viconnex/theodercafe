c3df744fda577d5ac37266618bb4dd757d42887e
Victor Lebrun <victorl@theodo.fr>
2019-09-04 00:47:03 +0200

    Add websocket for accumulus

---
+++ backend/src/accumulus/accumulus.gateway.ts
@@ -0,0 +1,17 @@
+import { WebSocketGateway, OnGatewayConnection, SubscribeMessage, WsResponse } from '@nestjs/websockets';
+import { Client } from 'socket.io';
+
+@WebSocketGateway(4002)
+export class AccumulusGateway implements OnGatewayConnection {
+    @SubscribeMessage('events')
+    handleConnection(client: Client, data: string): WsResponse {
+        console.log(data);
+        client.server.emit('nuage', 'voyage');
+        return { event: 'events', data: 'réponse' };
+    }
+    @SubscribeMessage('upload')
+    handleUpload(client: Client, data: string): void {
+        console.log(data);
+        client.server.emit('upload', data);
+    }
+}
--- backend/src/accumulus/accumulus.module.ts
+++ backend/src/accumulus/accumulus.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Accumulus } from './accumulus.entity';
 import { AccumulusRepository } from './accumulus.repository';
 import { AccumulusController } from './accumulus.controller';
+import { AccumulusGateway } from './accumulus.gateway';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Accumulus, AccumulusRepository])],
     controllers: [AccumulusController],
+    providers: [AccumulusGateway],
 })
 export class AccumulusModule {}
