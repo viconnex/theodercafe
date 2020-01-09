b96f38e97007280d32deeaf3dec89dc7bfee8c56
Victor Lebrun <victor.lebrun011@gmail.com>
2020-01-09 09:09:29 +0100

    Add map chart

--- frontend/src/pages/Map/Map.js
+++ frontend/src/pages/Map/Map.js
@@ -3,17 +3,20 @@ import { fetchRequestResponse } from 'services/api';
 import { useSnackbar } from 'notistack';
 import { USER_TO_QUESTIONS_CHOICES_URI } from 'utils/constants/apiConstants';
 
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
 const Map = () => {
   const [map, setMap] = useState(null);
   const { enqueueSnackbar } = useSnackbar();
 
   const fetchMap = async () => {
-    const response = await fetchRequestResponse({ uri: `/${USER_TO_QUESTIONS_CHOICES_URI}/user`, method: 'GET' }, 200, {
+    const response = await fetchRequestResponse({ uri: `/${USER_TO_QUESTIONS_CHOICES_URI}/map`, method: 'GET' }, 200, {
       enqueueSnackbar,
     });
     const data = await response.json();
     console.log(data);
-    setMap(map);
+    setMap(data.map);
   };
 
   useEffect(() => {
@@ -21,7 +24,32 @@ const Map = () => {
     // eslint-disable-next-line
   }, []);
 
-  return <div>{map}</div>;
+  if (!map) {
+    return <div>loading</div>;
+  }
+
+  const options = {
+    title: {
+      text: 'My chart',
+    },
+    series: [
+      {
+        type: 'scatter',
+        data: map.map(dot => ({
+          x: dot[0],
+          y: dot[1],
+          name: 'chico',
+          marker: {
+            height: 15,
+            width: 15,
+            symbol: 'url(https://lh3.googleusercontent.com/a-/AAuE7mCUFpMUIarQSeMITVXIOeLBBU8mnp-SPE2QHgD2)',
+          },
+        })),
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
 };
 
 export default Map;
