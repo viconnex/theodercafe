25217fd9fc874eda031c3ea9a3b47967ae0f2fd5
Victor Lebrun <victor.lebrun011@gmail.com>
2020-09-07 21:33:51 +0200

    remove react service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common'
 export class AppService {
     getHello(): string {
         console.log('salut le monde')
-        return 'Hello wooooorld!'
+        return 'couc'
     }
 }
