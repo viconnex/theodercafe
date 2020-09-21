345ad881b119766c22be50a3ad15a9735cca3f97
Victor Lebrun <victor.lebrun011@gmail.com>
2020-09-21 21:09:56 +0200

    Add post user controller

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserController } from 'src/user/user.controller'
 import { User } from './user.entity'
 import { UserRepository } from './user.repository'
 import { UserService } from './user.service'
@@ -7,6 +8,7 @@ import { UserService } from './user.service'
 @Module({
     imports: [TypeOrmModule.forFeature([User, UserRepository])],
     providers: [UserService],
+    controllers: [UserController],
     exports: [UserService],
 })
 export class UserModule {}
