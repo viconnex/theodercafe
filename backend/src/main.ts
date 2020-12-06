d7ffa06cbf7e1d18096510c88300d9153037b2e5
Victor Lebrun <victor.lebrun011@gmail.com>
2020-12-06 20:30:41 +0100

    add health check

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
     console.log('node_env', process.env.NODE_ENV)
-    console.log('JWT_SECRET_KEY', process.env.JWT_SECRET_KEY)
     const app = await NestFactory.create(AppModule)
     app.enableCors()
     await app.listen(8080)
