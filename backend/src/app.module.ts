e451ec554fb4ef7ba0527618204d2bc6bf037a3e
Victor Lebrun <victor.lebrun011@gmail.com>
2020-12-06 19:59:44 +0100

    parent 3291984ae7cc701d2b9268d0459484d9c7e8f54e author Victor Lebrun <victor.lebrun011@gmail.com> 1607281184 +0100 committer Victor Lebrun <victor.lebrun011@gmail.com> 1607453386 +0100
    update nestjs cli

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { UserModule } from './user/user.module'
 import { UserToQuestionChoiceModule } from './userToQuestionChoice/userToQuestionChoice.module'
 import { UserToQuestionVoteModule } from './userToQuestionVote/userToQuestionVote.module'
 import { QuestioningHistoricModule } from './questioningHistoric/questioningHistoric.module'
+import { HealthController } from './health/health.controller';
 
 @Module({
     imports: [
@@ -41,7 +42,7 @@ import { QuestioningHistoricModule } from './questioningHistoric/questioningHist
             },
         }),
     ],
-    controllers: [AppController],
+    controllers: [AppController, HealthController],
     providers: [AppService],
 })
 export class AppModule {}
