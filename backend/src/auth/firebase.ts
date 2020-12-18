5550fdf1bf555763ede546abdf5d2005e29432ed
Victor Lebrun <victor.lebrun011@gmail.com>
2020-12-18 18:58:10 +0100

    fix firebase token generation

--- backend/src/auth/firebase.ts
+++ backend/src/auth/firebase.ts
@@ -1,21 +1,22 @@
 import { sign } from 'jsonwebtoken'
 
-const issuingTime = Math.round(Date.now() / 1000)
+const issuingTime = Math.round(Date.now() / 1000 - 60)
 const expirationTime = issuingTime + 3600 // Maximum expiration time is one hour
 
 export const createFirebaseJWT = (id: number) => {
     const payload = {
-        iss: 'firebase-adminsdk-iglrm@maposaic-99785.iam.gserviceaccount.com',
-        sub: 'firebase-adminsdk-iglrm@maposaic-99785.iam.gserviceaccount.com',
+        alg: 'RS256',
+        iss: 'firebase-adminsdk-o2guq@theodercafe.iam.gserviceaccount.com',
+        sub: 'firebase-adminsdk-o2guq@theodercafe.iam.gserviceaccount.com',
         aud: 'https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit',
         iat: issuingTime,
         exp: expirationTime,
         uid: id,
     }
-    const secret = process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY
+    const secret = process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/g, '\n')
     if (secret === undefined) {
         throw Error('no firebase service account secret in env')
     }
 
-    return sign(payload, secret)
+    return sign(payload, secret, { algorithm: 'RS256' })
 }
