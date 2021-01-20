2a58b96745f06da6e14c3ab699e2f030dcdbf18f
Victor Lebrun <victor.lebrun011@gmail.com>
2021-01-20 17:15:32 +0100

    add frontend firebase prod project

--- frontend/src/services/firebase/initialiseFirebase.ts
+++ frontend/src/services/firebase/initialiseFirebase.ts
@@ -4,16 +4,27 @@ import 'firebase/firestore'
 import 'firebase/analytics'
 import 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBAnVOIji4axVKbeYQCxcfINzCy4ujMJqs',
-  authDomain: 'theodercafe.firebaseapp.com',
-  databaseURL: 'https://theodercafe.firebaseio.com',
-  projectId: 'theodercafe',
-  storageBucket: 'theodercafe.appspot.com',
-  messagingSenderId: '509723639859',
-  appId: '1:509723639859:web:50e3385a4bb93b054d2346',
-  measurementId: 'G-0L0YG5XCG2',
-}
+const firebaseConfig =
+  process.env.NODE_ENV === 'production'
+    ? {
+        apiKey: 'AIzaSyCVa4tgSTtQPZLZa0D0i672ewQl6-anDBY',
+        authDomain: 'theodercafe-302118.firebaseapp.com',
+        projectId: 'theodercafe-302118',
+        storageBucket: 'theodercafe-302118.appspot.com',
+        messagingSenderId: '893586425895',
+        appId: '1:893586425895:web:9d3c8dc0e05255e80fe602',
+        measurementId: 'G-8NPN5DR6SZ',
+      }
+    : {
+        apiKey: 'AIzaSyBAnVOIji4axVKbeYQCxcfINzCy4ujMJqs',
+        authDomain: 'theodercafe.firebaseapp.com',
+        databaseURL: 'https://theodercafe.firebaseio.com',
+        projectId: 'theodercafe',
+        storageBucket: 'theodercafe.appspot.com',
+        messagingSenderId: '509723639859',
+        appId: '1:509723639859:web:50e3385a4bb93b054d2346',
+        measurementId: 'G-0L0YG5XCG2',
+      }
 
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
