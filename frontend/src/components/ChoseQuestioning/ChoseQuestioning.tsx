34f992e83cb3fc492b60c5fa8ced836a89d02daf
Victor Lebrun <victor.lebrun011@gmail.com>
2021-01-24 12:07:56 +0100

    add data-grid with questions

--- frontend/src/components/ChoseQuestioning/ChoseQuestioning.tsx
+++ frontend/src/components/ChoseQuestioning/ChoseQuestioning.tsx
@@ -1,19 +1,21 @@
+import { DataGrid } from '@material-ui/data-grid'
+import useStyle from 'components/ChoseQuestioning/style'
 import { AdminQuestionResponse } from 'components/ChoseQuestioning/types'
 import React, { useEffect, useState } from 'react'
 import { fetchRequest } from 'services/api'
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'option1', headerName: 'Option 1', width: 130 },
-  { field: 'option2', headerName: 'Option 2', width: 130 },
-  { field: 'choice1count', headerName: 'Choix 1' },
-  { field: 'choice2count', headerName: 'Choix 2' },
-  { field: 'upVotes', headerName: 'Up votes' },
-  { field: 'downVotes', headerName: 'Down votes' },
+  { field: 'id', headerName: 'Id', width: 70 },
+  { field: 'option1', headerName: 'Option 1', width: 300 },
+  { field: 'option2', headerName: 'Option 2', width: 300 },
+  { field: 'choice1count', headerName: 'C1', width: 80 },
+  { field: 'choice2count', headerName: 'C2', width: 80 },
+  { field: 'upVotes', headerName: 'UpV' },
+  { field: 'downVotes', headerName: 'DownV' },
 ]
 
 const ChoseQuestioning = () => {
-  const [questions, setQuestions] = useState<AdminQuestionResponse[] | null>(null)
+  const [questions, setQuestions] = useState<AdminQuestionResponse[]>([])
 
   const fetchQuestions = async () => {
     const response = await fetchRequest({ uri: '/questions', method: 'GET', body: null, params: null })
@@ -25,7 +27,18 @@ const ChoseQuestioning = () => {
     void fetchQuestions()
   }, [])
 
-  return questions ? <div>coucou</div> : <div>Loading...</div>
+  const classes = useStyle()
+  return (
+    <div className={classes.container}>
+      <DataGrid
+        loading={questions.length === 0}
+        className={classes.root}
+        rows={questions}
+        columns={columns}
+        rowHeight={25}
+      />
+    </div>
+  )
 }
 
 export default ChoseQuestioning
---
+++ frontend/src/components/ChoseQuestioning/style.ts
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core'
+
+const useStyle = makeStyles(() => {
+  return {
+    container: {
+      width: '100%',
+      height: '100%',
+      padding: '16px 0',
+    },
+    root: {
+      backgroundColor: 'white',
+    },
+  }
+})
+
+export default useStyle
