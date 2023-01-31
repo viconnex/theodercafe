a6373026f9ee7c11d8e4928c6a90e1ea8aee3968
Victor Lebrun <victor.lebrun011@gmail.com>
2023-01-31 16:51:04 +0100

    add info to admin user edit page

--- frontend/src/admin/Users/UserEdit.js
+++ frontend/src/admin/Users/UserEdit.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { BooleanInput, Edit, SimpleForm } from 'react-admin'
+import { BooleanInput, Edit, SimpleForm, TextInput } from 'react-admin'
 
 const UserEdit = (props) => (
   <Edit {...props}>
     <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput disabled source="email" label="email" />
+      <TextInput disabled source="givenName" label="Firstname" />
+      <TextInput disabled source="familyName" label="Lastname" />
       <BooleanInput source="isActive" label="Active ?" />
       <BooleanInput source="isAdmin" label="Admin ?" />
     </SimpleForm>
