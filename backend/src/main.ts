a3f9721321baef09a040a6c22af304f5a8a1b9cd
Alexandre Pierre <alexandrep@hokla.com>
2024-11-08 09:54:09 +0100

    docs: ✏ setup swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
@@ -6,6 +7,17 @@ async function bootstrap(): Promise<void> {
     console.log('BACKEND_URL', process.env.BACKEND_URL)
     const app = await NestFactory.create(AppModule)
     app.enableCors()
+
+    // Swagger setup
+    const config = new DocumentBuilder()
+        .setTitle('Theodercafe API')
+        .setDescription(
+            'This is the Theodercafe API description. You can find here all the endpoints to interact with the Theodercafe backend',
+        )
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document)
+
     await app.listen(8080)
 }
 // eslint-disable-next-line
